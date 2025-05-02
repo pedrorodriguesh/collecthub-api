6be658384a5d6631daa59d471c8318771f638be0
Pedro Henrique Rodrigues <pedrorodrigueshdev@gmail.com>
2025-05-02 20:54:58 -0300

    feat: migration auth-links table

--- src/db/schema/index.ts
+++ src/db/schema/index.ts
@@ -1,1 +1,2 @@
+export * from './auth-links';
 export * from './users';
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { Pool } from 'pg';
+import * as schema from './db/schema';
 import { env } from './env';
 
 const pool = new Pool({
 	connectionString: env.DATABASE_URL,
 });
 
-export const db = drizzle(pool);
+export const db = drizzle(pool, { schema });
