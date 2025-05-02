c6b71a4059c7959f2bc550e6ffed7cdb26aaaeb7
Pedro Henrique Rodrigues <pedrorodrigueshdev@gmail.com>
2025-05-02 20:04:50 -0300

    feat: create user route

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,8 +1,48 @@
-import { Elysia } from 'elysia';
+import { swagger } from '@elysiajs/swagger';
+import { Elysia, t } from 'elysia';
+import { db } from '..';
+import { users } from '../db/schema';
 
-const app = new Elysia().get('/', () => {
-	return 'Hello World';
-});
+const app = new Elysia()
+	.use(
+		swagger({
+			documentation: {
+				info: {
+					title: 'CollectHub API',
+					description: 'API for CollectHub',
+					version: '1.0.0',
+				},
+			},
+		}),
+	)
+	.post(
+		'/users',
+		async ({ body, set }) => {
+			const { name, email } = body;
+
+			const [user] = await db
+				.insert(users)
+				.values({
+					name,
+					email,
+				})
+				.returning({
+					id: users.id,
+				});
+
+			set.status = 204;
+
+			return {
+				user,
+			};
+		},
+		{
+			body: t.Object({
+				name: t.String(),
+				email: t.String(),
+			}),
+		},
+	);
 
 app.listen(3333, () => {
 	console.log('👨🏻‍💻 Server is running');
